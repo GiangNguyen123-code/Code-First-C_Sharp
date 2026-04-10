5ab55c016689c73f3a88cc19f92dd6e7c3e65599
Nguyen Do Huu Giang <nguyendohuugiang2006@gmail.com>
2026-04-10 21:30:00 +0700

    Bước 6: Tạo View - Khi dùng MVC Controller with views, using Entity Framework ở bước 5 thì tự động tạo các file view của các thao tác cơ bản

---
+++ CodeFirst_Student/Data/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using CodeFirst_Student.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CodeFirst_Student.Data
+{
+        public class AppDbContext : DbContext
+        {
+            public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+            {
+
+            }
+            public DbSet<Student> Students { get; set; }
+        }
+}
