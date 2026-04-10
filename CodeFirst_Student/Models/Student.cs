fe5eaab9037ba77937fb9377c78d1e42c43313df
Nguyen Do Huu Giang <nguyendohuugiang2006@gmail.com>
2026-04-10 20:44:37 +0700

    Bước 2: Tạo 1 file class mới trong lớp Model, đặt tên là Student.cs

---
+++ CodeFirst_Student/Models/Student.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace CodeFirst_Student.Models
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public String Name { get; set; }
+        public int Age { get; set; }
+    }
+}
